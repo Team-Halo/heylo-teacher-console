affafcf7d550aebeaabbc06009edd3cf5fa51bb6
Stanley Chan <0010stanleychan@gmail.com>
2021-01-24 23:20:22 +0800

    edited publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,28 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: '/',
+  publicPath: '',
   css: {
     loaderOptions: {
       sass: {
         sassOptions: {
-          includePaths: ['./node_modules', './src/assets'],
-        },
-      },
-    },
+          includePaths: ['./node_modules', './src/assets']
+        }
+      }
+    }
   },
   configureWebpack: {
     resolve: {
       alias: {
         '@themeConfig': path.resolve(__dirname, 'themeConfig.js'),
         '@core': path.resolve(__dirname, 'src/@core'),
-        '@validations': path.resolve(__dirname, 'src/@core/utils/validations/validations.js'),
-        '@axios': path.resolve(__dirname, 'src/libs/axios'),
-      },
-    },
+        '@validations': path.resolve(
+          __dirname,
+          'src/@core/utils/validations/validations.js'
+        ),
+        '@axios': path.resolve(__dirname, 'src/libs/axios')
+      }
+    }
   },
   chainWebpack: config => {
     config.module
@@ -38,10 +41,10 @@ module.exports = {
           'b-card-img': 'src',
           'b-card-img-lazy': ['src', 'blank-src'],
           'b-carousel-slide': 'img-src',
-          'b-embed': 'src',
+          'b-embed': 'src'
         }
         return options
       })
   },
-  transpileDependencies: ['vue-echarts', 'resize-detector'],
+  transpileDependencies: ['vue-echarts', 'resize-detector']
 }
