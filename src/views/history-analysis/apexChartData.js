cd451a2b87035e3930a53baa0678c09e26da3bbe
Stanley Chan <0010stanleychan@gmail.com>
2021-01-24 19:23:35 +0800

    Editing timeline

--- src/views/history-analysis/apexChartData.js
+++ src/views/history-analysis/apexChartData.js
@@ -1,98 +1,88 @@
 // colors
 const chartColors = {
-  column: {
-    series1: "#826af9",
-    series2: "#d2b0ff",
-    bg: "#f8d3ff",
-  },
-  success: {
-    shade_100: "#7eefc7",
-    shade_200: "#06774f",
-  },
   donut: {
-    series1: "#ffe700",
-    series2: "#00d4bd",
-    series3: "#826bf8",
-    series4: "#2b9bf4",
-    series5: "#FFA1A1",
-  },
-  area: {
-    series3: "#a4f8cd",
-    series2: "#60f2ca",
-    series1: "#2bdac7",
-  },
-};
+    series1: '#ffc600',
+    series2: '#00d4bd',
+    series3: '#826bf8',
+    // series4: '#2b9bf4',
+    series5: '#FFA1A1'
+  }
+}
 
 export default {
   donutChart: {
-    series: [85, 16, 50, 50],
+    series: [10, 20, 20, 50],
     chartOptions: {
       legend: {
-        show: true,
-        position: "bottom",
-        fontSize: "14px",
-        fontFamily: "Montserrat",
+        show: false,
+        position: 'bottom',
+        fontSize: '14px',
+        fontFamily: 'Lato'
       },
       colors: [
         chartColors.donut.series1,
         chartColors.donut.series5,
         chartColors.donut.series3,
-        chartColors.donut.series2,
+        chartColors.donut.series2
       ],
       dataLabels: {
-        enabled: true,
+        enabled: false,
         formatter(val) {
           // eslint-disable-next-line radix
-          return `${parseInt(val)}%`;
-        },
+          return `${parseInt(val)}%`
+        }
       },
       plotOptions: {
         pie: {
+          customScale: 0.8,
           donut: {
+            size: '90%',
             labels: {
               show: true,
               name: {
-                fontSize: "2rem",
-                fontFamily: "Montserrat",
+                fontSize: '5rem',
+                fontFamily: 'Lato'
               },
               value: {
-                fontSize: "1rem",
-                fontFamily: "Montserrat",
+                fontSize: '2rem',
+                fontFamily: 'Lato',
                 formatter(val) {
                   // eslint-disable-next-line radix
-                  return `${parseInt(val)}%`;
-                },
+                  return `${parseInt(val)}%`
+                }
               },
               total: {
                 show: true,
-                fontSize: "1.5rem",
-                label: "Operational",
-                formatter() {
-                  return "31%";
-                },
-              },
-            },
-          },
-        },
+                fontSize: '1.5rem',
+                label: 'Operational',
+                formatter(val) {
+                  return val
+                  // eslint-disable-next-line radix
+                  // return w.globals.seriesTotals.reduce((a, b) => return a + b, 0)
+                }
+              }
+            }
+          }
+        }
       },
-      labels: ["Operational", "Networking", "Hiring", "R&D"],
+      labels: ['Sleepy', 'Neutral', 'Confused', 'Happy'],
       responsive: [
         {
           breakpoint: 992,
           options: {
             chart: {
-              height: 380,
+              height: 380
             },
             legend: {
-              position: "bottom",
-            },
-          },
+              position: 'bottom'
+            }
+          }
         },
         {
           breakpoint: 576,
           options: {
             chart: {
-              height: 320,
+              height: 320
             },
             plotOptions: {
               pie: {
@@ -100,24 +90,24 @@ export default {
                   labels: {
                     show: true,
                     name: {
-                      fontSize: "1.5rem",
+                      fontSize: '1.5rem'
                     },
                     value: {
-                      fontSize: "1rem",
+                      fontSize: '1rem'
                     },
                     total: {
-                      fontSize: "1.5rem",
-                    },
-                  },
-                },
-              },
+                      fontSize: '1.5rem'
+                    }
+                  }
+                }
+              }
             },
             legend: {
-              show: true,
-            },
-          },
-        },
-      ],
-    },
-  },
-};
+              show: true
+            }
+          }
+        }
+      ]
+    }
+  }
+}
